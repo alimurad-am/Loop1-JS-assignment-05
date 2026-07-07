3fa4befb11969d25273c2dd10f27db4ad24c32ea
Ali Murad <samejoalimurad321@gmail.com>
2026-07-07 18:59:32 +0500

    Assignment updated

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let number = 5;
 
-for ( let i = 1; i <= 10; i++){
-    console.log("5 *", i,"=", number*i );
+for ( let i = 1; i <= 12; i++){
+    console.log(number + " x " + i +"= " + number*i);
+    // Isko hm is method se bhe kr sakte hen //
+    
     // console.log(`${number} * ${i} = ${number * i}`);
 }
